abd1973129602047821c6a7cb268e839ade84156
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-07-25 12:12:22 +0200

    Creazione di un array contenente i dati delle attività e utilizzato map per creare dinamicamente i componenti TodoItem

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,31 @@
+// import
 import TodoItem from "./TodoItem";
 
+// dati
+const todos = [
+    {
+        id: 1,
+        done: false,
+        text: "Prima Attività",
+    },
+    {
+        id: 2,
+        done: true,
+        text: "Seconda Attività",
+    },
+    {
+        id: 3,
+        done: false,
+        text: "Terza Attività",
+    },
+];
+
+// componenti
 export default function TodoList() {
+    const todoItems = todos.map((t) => (<TodoItem key={t.id} done={t.done} text={t.text} />));
     return (
         <ul className="list-group pb-3">
-            <TodoItem done={false} text="Prima attività" />
-            <TodoItem done={true} text="Seconda attività" />
-            <TodoItem done={false} text="Terza attività" />
+            {todoItems}
         </ul>
     );
 }
